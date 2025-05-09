00fd6c533852fb1c1583de45e000cf1bf141b6e8
lakshya369 <groverlakshya.25.lg@gmail.com>
2025-05-09 05:59:40 +0530

    shifted the permisions for add prospects from user to admin control

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,12 @@ const {
   createAdmin,
   getAllAdmins,
   deleteAdmin,
+  addProspect,
+  addProspectsByExcel,
 } = require("../controllers/adminController");
 const { protect, authorize } = require("../middleware/auth");
 const uploadPhotoMiddleware = require("../middleware/uploadPhotoMiddleware");
+const { uploadExcel } = require("../middleware/uploadExcelMiddleware");
 
 // Routes
 router.post(
@@ -18,5 +21,29 @@ router.post(
 );
 router.get("/getAllAdmins", protect, authorize("admin"), getAllAdmins);
 router.delete("/delete/:id", protect, authorize("admin"), deleteAdmin);
+router.post(
+  "/addProspect",
+  protect,
+  authorize("admin"),
+  (req, res, next) => {
+    uploadPhotoMiddleware.single("Photo")(req, res, function (err) {
+      if (err) {
+        console.error("Multer error:", err.message);
+        return res.status(400).json({ message: err.message });
+      } else {
+        console.log("Multer success:", req.file); // Log the file information
+      }
+      next();
+    });
+  },
+  addProspect
+);
+router.post(
+  "/addProspectByExcel",
+  protect,
+  authorize("admin"),
+  uploadExcel.single("ProspectsExcelData"),
+  addProspectsByExcel
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,42 +3,13 @@ const router = express.Router();
 const {
   getAllUsers,
   deleteUser,
-  addProspect,
-  addProspectsByExcel,
   getAllProspects,
 } = require("../controllers/userController");
 const { protect, authorize } = require("../middleware/auth");
-const { uploadExcel } = require("../middleware/uploadExcelMiddleware");
-// const {uploadExcelMiddleware} = require("../middleware/uploadExcelMiddleware");
-const uploadPhotoMiddleware = require("../middleware/uploadPhotoMiddleware");
 
 // Routes
 router.get("/getAllUsers", protect, authorize("admin"), getAllUsers);
 router.delete("/delete/:id", protect, authorize("admin"), deleteUser);
-router.post(
-  "/addProspect",
-  protect,
-  authorize("user", "admin"),
-  (req, res, next) => {
-    uploadPhotoMiddleware.single("Photo")(req, res, function (err) {
-      if (err) {
-        console.error("Multer error:", err.message);
-        return res.status(400).json({ message: err.message });
-      } else {
-        console.log("Multer success:", req.file); // Log the file information
-      }
-      next();
-    });
-  },
-  addProspect
-);
-router.post(
-  "/addProspectByExcel",
-  protect,
-  authorize("user", "admin"),
-  uploadExcel.single("ProspectsExcelData"),
-  addProspectsByExcel
-);
 router.get(
   "/getAllProspects",
   protect,
